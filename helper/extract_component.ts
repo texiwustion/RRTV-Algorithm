d39e34c08ebb8786f6f74bb82115b92198c25286
texiwustion <w6666s@outlook.com>
2023-07-02 09:21:49 +0800

    ✨ feat(EC): 创建用例与分析结构
    书写所有可能用到的顶层 find 语句 \n 打开 ast explorer 以实时 transformer 分析

--- helper/extract_component.ts
+++ helper/extract_component.ts
@@ -1,1 +1,41 @@
 import * as j from 'jscodeshift'
+
+/**
+ * structure: VD (RS (JF (JE (JE JE))))
+ */
+const text = `const App = () => {
+    return (
+        <>
+            <div>
+                <button id="3"></button>
+                <p>this is a para</p>
+            </div>
+        </>
+    )
+}`
+
+/**
+ * the range of selection, present by offset
+ */
+const range = { start: 3, end: 19 }
+
+export default function transformer(file: j.FileInfo, api: j.API) {
+    const j: j.JSCodeshift = api.jscodeshift;
+    const root: j.Collection = j(file.source);
+
+    {
+        const jsxElements: j.Collection = root.findJSXElements()
+        console.log('line:22 jsxElements::: ', jsxElements);
+    }
+
+    {
+        const returnStatements: j.Collection = root.find(j.ReturnStatement)
+        console.log('line:27 returnStatements::: ', returnStatements);
+    }
+
+    {
+        const variableDeclaration: j.Collection = root.find(j.VariableDeclaration)
+        console.log('line:32 variableDeclaration::: ', variableDeclaration);
+    }
+    return root.toSource();
+}
