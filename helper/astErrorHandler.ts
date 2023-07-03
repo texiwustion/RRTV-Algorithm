13605dadd05c2127ff6e7c60b22c19c48ae1d2a8
texiwustion <w6666s@outlook.com>
2023-07-03 20:22:26 +0800

    ✨ feat(EC): 添加 astErrorHandler

---
+++ helper/astErrorHandler.ts
@@ -0,0 +1,33 @@
+import { getStandardDOM } from "./getStandardDOM"
+import * as j from 'jscodeshift'
+
+/**
+ * use babel analyser for making exactly complete jsxElement
+ * @param text String, a range of text from USER selection
+ * @returns null | root: j.Collection (a complete jsxElement)
+ */
+export function astHandle(text: string): j.Collection<any> | null {
+    const result = getStandardDOM.byText(text)
+    if (result === null) {
+        return null
+    }
+    let root: j.Collection<any> | null = null
+    try {
+        root = j(result.rawText)
+    }
+    catch (e) {
+        try {
+            root = j(result.newText)
+        }
+        catch (e) {
+            console.log("[astErrorHandler]Line24::: Error: 所选区域内无完整 DOM 元素！")
+        }
+    }
+
+    if (root === null) {
+        return null
+    }
+    else {
+        return root
+    }
+}
