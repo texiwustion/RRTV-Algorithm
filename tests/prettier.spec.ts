6a7204d5a992e098f7631674100b1ee4d830e6ac
texiwustion <w6666s@outlook.com>
2023-07-07 15:07:20 +0800

    ✨ feat(EC): 添加 prettier 及测试

---
+++ tests/prettier.spec.ts
@@ -0,0 +1,10 @@
+import { prettierCode } from "../helper/prettier"
+
+it("test prettier", async () => {
+    const code = `//格式故意搞乱，不对齐！
+function uglyCode () {
+                    console.log("this is an ugly function!") 
+ }`
+    expect(await prettierCode(code)).toMatchSnapshot()
+})
+
