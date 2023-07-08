b3cbedabe29877eaf1d30afe35b06e7f1a1b8554
texiwustion <w6666s@outlook.com>
2023-07-08 23:31:09 +0800

    ✨ feat(prettier): prittier 的测试

--- tests/prettier.spec.ts
+++ tests/prettier.spec.ts
@@ -1,10 +1,9 @@
-import { prettierCode } from "../helper/prettier"
+import { prettierCode } from "../src/helper/prettier";
 
 it("test prettier", async () => {
     const code = `//格式故意搞乱，不对齐！
 function uglyCode () {
                     console.log("this is an ugly function!") 
- }`
-    expect(await prettierCode(code)).toMatchSnapshot()
-})
-
+ }`;
+    expect(await prettierCode(code)).toMatchSnapshot();
+});
