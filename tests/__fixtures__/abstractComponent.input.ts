549dd0e9936ca7e36b8a7dbb05fd92aa39ed327f
texiwustion <w6666s@outlook.com>
2023-07-08 23:30:47 +0800

    ✨ feat(AC): 添加 AC 的测试

---
+++ tests/__fixtures__/abstractComponent.input.ts
@@ -0,0 +1,35 @@
+export const code = {
+    test1: `const TestSim = () => {
+    return <>
+        <hello>
+            <button id="1" me="2"></button>
+            <button you="3" type="4"></button>
+        </hello>
+        <div>
+            <button id="1" me="2"></button>
+            <button you="3" type="4"></button>
+        </div>
+        <p></p>
+    </> 
+    }
+    const TestSim2 = () => {
+    return <>
+        <code></code>
+        <div>
+            <button id="1" me="2"></button>
+            <button you="3" type="4"></button>
+        </div>
+        <hello>
+            <button id="1" me="2"></button>
+            <button you="3" type="4"></button>
+        </hello>
+    </> 
+    }
+
+`,
+};
+
+export const range_54_127 = {
+    start: 54,
+    end: 127,
+};
